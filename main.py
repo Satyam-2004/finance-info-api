a69250ff04ecda64229d8d60a0bdde63e2f7de95
Satyam Sharma <skpsharma2004@gmail.com>
2026-01-24 09:03:58 +0530

    Added endpoint to save stock data to csv

--- main.py
+++ main.py
@@ -1,38 +1,19 @@
 from fastapi import FastAPI
+import pandas as pd
+import yfinance as yf
+from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-def root():
-    return {"status": "ok"}
+@app.get("/save-stock/{ticker}")
+def save_stocks_data(ticker: str, period: Optional[str] = "1mo"):
+    
+    data = yf.download(ticker, period=period)
 
-@app.get("/stocks/{symbol}")
-def get_stock(symbol: str):
-    return {"symbol": symbol}
+    if data.empty:
+        return {"message" : "No data found for ticker"}
+    
+    filename = f"{ticker}_{period}.csv"
+    data.to_csv(filename)
 
-@app.get("/stocks/{symbol}/history")
-def get_stock_history(symbol: str, period: str = "1mo"):
-    return {
-        "symbol": symbol,
-        "period" : period
-    }
-
-#################################
-
-@app.get("/item/{item_id}")
-async def read_item(item_id: str, q: str | None = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
-
-#################################
-@app.get("/items/{items_id}")
-async def read_items(items_id: str, q : str | None = None , short: bool = False):
-    item = {"items_id": items_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item 
+    return {"message" : f"Stock data for {ticker} has been for {period} period"}
