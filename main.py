00aa435f0aa1e9459038bb53007b88031d07283d
Satyam Sharma <skpsharma2004@gmail.com>
2026-01-24 09:55:26 +0530

    Added endpoint to download the stock data with formatted name

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 import pandas as pd
 import yfinance as yf
 from typing import Optional
@@ -17,3 +18,13 @@ def save_stocks_data(ticker: str, period: Optional[str] = "1mo"):
     data.to_csv(filename)
 
     return {"message" : f"Stock data for {ticker} has been for {period} period"}
+
+@app.get("/display")
+def display_data(ticker: str, period: str):
+    csv_file = f"{ticker}_{period}.csv"
+
+    return FileResponse(
+        path=csv_file,
+        media_type="text/csv",
+        filename="data_download.csv"
+    )
