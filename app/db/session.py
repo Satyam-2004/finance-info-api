615335c9632408a4edcc10554bc4ab7970761f88
Satyam Sharma <skpsharma2004@gmail.com>
2026-01-31 08:20:23 +0530

    current db codebase

---
+++ app/db/session.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine, String, Integer, text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, sessionmaker
+
+engine = create_engine("sqlite:///mydb.db", echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+
+with SessionLocal as session:
+    
